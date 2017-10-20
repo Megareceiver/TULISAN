5308f0513135d2bf35b192996af96272d710bbb9
megan <megareceiver@gmail.com>
2017-10-21 01:28:59 +0700

    up

--- assets/js/syncard.js
+++ assets/js/syncard.js
@@ -1,63 +1,72 @@
 /*syncard js*/
 $(function(){
 	headerAdmin();
-	menuAdmin();
+	pageAdmin();
+	footerAdmin();
 });
 
 
 function headerAdmin(){
 	var html =
-	'<div class="row">'+
-		'<div class="col-md-3">'+
-			'<div class="logo"><img class="big-image" src="../assets/PICS/tulisan.png" /></div>'+
-		'</div>'+
-		'<div class="col-md-9">'+
-			'<div class="header-title"><h2>Product - Departement</h2></div>'+
-			'<div class="header-menu fontserif">' + 
-				'<ul>' + 
-					'<li><a href=#>View Store</a> &nbsp; &nbsp;|</li>' + 
-					'<li><img src="../assets/PICS/pattern3.jpg"></li>' +
-					'<li>Khrisni Swandayani</li>' + 
-					'<li><a href="#"><i class="fa fa-cog fa-o"></i></a></li>' + 
-				'</ul>'+
+	'<div class="content-frame">'+
+		'<div class="row">'+
+			'<div class="col-md-3">'+
+				'<div class="logo"><img class="big-image" src="../assets/PICS/tulisan.png" /></div>'+
 			'</div>'+
-		'</div>'+
-	'</div><div class="clearfix"></div>';
+			'<div class="col-md-9">'+
+				'<div class="header-title"><h2>Product - Departement</h2></div>'+
+				'<div class="header-menu fontserif">' + 
+					'<ul>' + 
+						'<li><a href=#>View Store</a> &nbsp; &nbsp;|</li>' + 
+						'<li><img src="../assets/PICS/pattern3.jpg"></li>' +
+						'<li>Khrisni Swandayani</li>' + 
+						'<li><a href="#"><i class="fa fa-cog fa-o"></i></a></li>' + 
+					'</ul>'+
+				'</div>'+
+			'</div>'+
+		'</div><div class="clearfix"></div>' +
+	'</div>';
 
 	$("header.parent-admin").html(html);
 }
 
-function menuAdmin(){
+function pageAdmin(){
 	var html =
-	'<div class="row">'+
-		'<div class="col-md-3 left-nav">'+
-			'<div class="list-nav">'+
-				'<ul>' +
-					'<li><a href="#">DASHBOARD</a></li>'+
-					'<li><a href="#">PRODUCT</a></li>'+
-					'<li><a href="#">CMS</a></li>'+
-					// '<li><a href="#">SYSTEM SETTING</a></li>'+
-					// '<li><a href="#">USERS BLOG</a></li>'+
-					'<li><a href="#">CUSTOMERS</a></li>'+
-					'<li><a href="#">VENDORS</a></li>'+
-					'<li><a href="#">ORDER</a></li>'+
-				'</ul>' +
+	'<div class="content-frame">'+
+		'<div class="row">'+
+			'<div class="col-md-3 left-nav">'+
+				'<div class="list-nav">'+
+					'<ul>' +
+						'<li><a href="#">DASHBOARD</a></li>'+
+						'<li><a href="#">PRODUCT</a></li>'+
+						'<li><a href="#">CMS</a></li>'+
+						// '<li><a href="#">SYSTEM SETTING</a></li>'+
+						// '<li><a href="#">USERS BLOG</a></li>'+
+						'<li><a href="#">CUSTOMERS</a></li>'+
+						'<li><a href="#">VENDORS</a></li>'+
+						'<li><a href="#">ORDER</a></li>'+
+					'</ul>' +
+				'</div>'+
 			'</div>'+
-		'</div>'+
-		'<div class="col-md-9 top-nav">'+
-			'<ul>' +
-				'<li><a href="#">Departement</a></li>'+
-				'<li><a href="#">Item</a></li>'+
-				'<li><a href="#">Customer</a></li>'+
-				'<li><a href="#">Shipping Option</a></li>'+
-				'<li><a href="#">Payment Method</a></li>'+
-				'<li><a href="#">Discounts</a></li>'+
-				'<li><a href="#">Catalog Price Rule</a></li>'+
-			'</ul>' +
-		'</div>'+
-		'<div class="col-md-9 main-content row">'+
-		'</div>'+
-	'</div><div class="clearfix"></div>';
+			'<div class="col-md-9 col-md-offset-3">'+
+				'<div class="col-md-12 top-nav">'+
+					'<ul>' +
+						'<li><a href="#">Departement</a></li>'+
+						'<li><a href="#">Item</a></li>'+
+						'<li><a href="#">Customer</a></li>'+
+						'<li><a href="#">Shipping Option</a></li>'+
+						'<li><a href="#">Payment Method</a></li>'+
+						'<li><a href="#">Discounts</a></li>'+
+						'<li><a href="#">Catalog Price Rule</a></li>'+
+					'</ul>' +
+				'</div>'+
+				'<div class="col-md-12 main-content">'+
+				'</div>'+
+				'<div class="col-md-12 main-footer">'+
+				'</div>'+
+			'</div>'+
+		'</div><div class="clearfix"></div>' +
+	'</div>';
 
 	$("main.parent-admin").html(html);
 }
@@ -65,5 +74,5 @@ function menuAdmin(){
 function footerAdmin(){
 	var html = "Copyright © 2014 Tulisan LLC. All Rights Reserved.";
 
-	$("footer.parent-admin").html(html);
+	$(".main-footer").html(html);
 }

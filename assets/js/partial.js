ca7720876670cd9a50853b5faa6963018241fbcc
megareceiver7 <32474428+megareceiver7@users.noreply.github.com>
2017-10-21 16:09:16 +0700

    up

--- assets/js/partial.js
+++ assets/js/partial.js
@@ -679,3 +679,42 @@ function countryListOption(){
 
 	return html;
 }
+
+/* =============================================================================================== */
+/* time converter */
+function timeSince(date) {
+	var seconds = Math.floor((new Date() - date) / 1000);
+	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	if (seconds < 5){
+		return "Just now";
+	}else if (seconds < 60){
+		return seconds + " seconds ago";
+	}
+	else if (seconds < 3600) {
+		minutes = Math.floor(seconds/60)
+		if(minutes > 1)
+			return minutes + " minutes ago";
+		else
+			return "1 minute ago";
+	}
+	else if (seconds < 86400) {
+		hours = Math.floor(seconds/3600)
+		if(hours > 1)
+			return hours + " hours ago";
+		else
+			return "1 hour ago";
+	}
+	//2 days and no more
+	else if (seconds < 172800) {
+		days = Math.floor(seconds/86400)
+		if(days > 1)
+			return days + " days ago";
+		else
+			return "1 day ago";
+	}
+	else{
+
+		//return new Date(time).toLocaleDateString();
+		return date.getDate().toString() + " " + months[date.getMonth()] + ", " + date.getFullYear();
+	}
+}
